52b4b00d2bbc0fa21a1aee820ac846e254f7deed
Paweł Szulc <paul.szulc@gmail.com>
2016-03-30 00:44:24 +0200

    Add JsonWriter.write implementation

--- src/main/scala/json/Json.scala
+++ src/main/scala/json/Json.scala
@@ -5,11 +5,21 @@ case class JsonObject(elem: Map[String, JsonValue]) extends JsonValue
 case class JsonArray(elems: List[JsonValue]) extends JsonValue
 case class JsonString(value: String) extends JsonValue
 case class JsonNumber(value: BigDecimal) extends JsonValue
-case object JsonNull extends JsonValue
 
 
 object JsonWriter {
-  def write(json: JsonValue): String = json.toString
+  def write(json: JsonValue): String = json match {
+    case JsonObject(elems) =>
+      val entries = for {
+        (key, value) <- elems
+      } yield s""""$key: ${write(value)}""""
+      "{" + entries.mkString(", ") + "}"
+    case JsonArray(elems) =>
+      val entries = elems.map(write)
+      "[" + entries.mkString(", ") + "]"
+    case JsonString(value) => s""""$value""""
+    case JsonNumber(value) => value.toString
+  }
 }
 
 trait JsonSerializable {
